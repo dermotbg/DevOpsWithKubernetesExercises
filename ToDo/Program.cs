c4c86a809dae6f1ce89d85a97b0f78677340a5f1
dermotbg <dermotbateman@gmail.com>
2024-07-20 09:58:44 +0300

    done 1.05: accessible via port-forward

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -2,8 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 // set port number with export PORT=<PORTNUMBER> command
-var PORT =  Environment.GetEnvironmentVariable("PORT") ?? config["Kestrel:Endpoints:Http:Url"];
+var PORT = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+
+builder.WebHost.UseUrls($"http://localhost:{PORT}");
+
 Console.WriteLine($"Server running on PORT: {PORT}");
 
 var app = builder.Build();
+app.UseStaticFiles();
+
+app.MapGet("/", () => 
+{
+  return Results.File("index.html", "text/html");
+});
+
 app.Run();
