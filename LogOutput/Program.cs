b2f5591bac9d40fd88807ae5f30a673f9f78caf7
dermotbg <dermotbateman@gmail.com>
2024-07-22 10:50:38 +0300

    done: 1.07

--- LogOutput/Program.cs
+++ LogOutput/Program.cs
@@ -1,7 +1,33 @@
-﻿var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+﻿var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
-while (await timer.WaitForNextTickAsync())
+// set port number with export PORT=<PORTNUMBER> command
+var PORT = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+
+builder.WebHost.UseUrls($"http://0.0.0.0:{PORT}");
+
+Console.WriteLine($"Server running on PORT: {PORT}");
+
+var app = builder.Build();
+app.UseStaticFiles();
+
+app.MapGet("/", async () => 
 {
-  Console.WriteLine($"{DateTime.Now}: {Guid.NewGuid()}");
-}
+  var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
 
+  string output = "Generating Hash...";
+
+  Console.WriteLine(output);
+
+  while (await timer.WaitForNextTickAsync())
+  {
+    output = $"{DateTime.Now}: {Guid.NewGuid()}";
+    return output;
+    // return Results.Content($"{DateTime.Now}: {Guid.NewGuid()}"); 
+  }
+  
+  return output;
+
+});
+
+app.Run();
