749cd80481741404855f5a2a0beca1cda5ee1157
dermotbg <dermotbateman@gmail.com>
2024-07-24 09:33:20 +0300

    chore: rename PingPongApp dir

---
+++ PingPongApp/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// set port number with export PORT=<PORTNUMBER> command
+var PORT = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+
+builder.WebHost.UseUrls($"http://0.0.0.0:{PORT}");
+
+Console.WriteLine($"Server running on PORT: {PORT}");
+
+var app = builder.Build();
+
+app.MapGet("/pingpong", () => 
+{
+  return Results.Content($"Pong {CounterService.IncrementCounter()}");
+  
+});
+
+app.Run();
+
+public static class CounterService
+{
+  private static int _counter = 0;
+  public static int IncrementCounter()
+  {
+    return _counter++;
+  }
+}
