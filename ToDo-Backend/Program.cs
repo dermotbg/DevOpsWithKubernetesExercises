1044d697b3ac1ffa73daddcfda45eb1df4bd5bd9
dermotbg <dermotbateman@gmail.com>
2024-07-28 10:37:53 +0300

    Backend application built

---
+++ ToDo-Backend/Program.cs
@@ -0,0 +1,60 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var PORT = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.UseUrls($"http://0.0.0.0:{PORT}");
+
+Console.WriteLine($"Server running on PORT: {PORT}");
+
+var app = builder.Build();
+
+app.MapGet("/todos", () => 
+{
+  return Results.Json(ToDoService.Get());
+});
+
+app.MapPost("/todos", (string newTodo) => 
+{
+  if(newTodo is null)
+  {
+    throw new InvalidOperationException("New todo must have a value");
+  }
+
+  try
+  {
+    ToDoService.Add(newTodo);
+  }
+  catch (System.Exception e)
+  {
+    Console.WriteLine("An error occurred " + e.Message);
+    throw new InvalidOperationException($"An error occurred  + {e.Message}");
+  }
+
+  return Results.Ok();
+});
+
+if(app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.Run();
+
+
+public static class ToDoService
+{
+  private static List<string>? _todos = new List<string>{};
+  public static  List<string> Get()
+  {
+    return _todos;
+  }
+  public static List<string> Add(string newTodo)
+  {
+    _todos.Add(newTodo);
+    return _todos;
+  }
+}
